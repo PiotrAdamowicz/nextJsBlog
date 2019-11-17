f6d75c7ffea7b69fa747ffbf49dfcf7d7cfcb091
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2019-11-17 21:10:36 +0100

    Added Punk Pooh img

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Link from "next/link";
 import Layout from "../components/layout";
+import poohImg from "../img/pooh.jpg";
 
 const Index = () => (
   <Layout>
     <h1>Hello Jacob</h1>
+    <img src={poohImg}></img>
   </Layout>
 );
 
