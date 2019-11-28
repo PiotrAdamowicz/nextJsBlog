8e36fc5eb61b65c2af2b6a2f945372864b18ae55
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2019-11-28 21:08:39 +0100

    Up to date with Master branch. Working on RWD

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
-import Layout from '../components/layout'
+import Layout from "../components/layout";
+import UnderConstruction from "../components/underConstruction";
 
 export default function About() {
-    return (
-        <Layout>
-            <div>
-                <h2>This is the about page</h2>
-            </div>
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <div>
+        <h2>This is the about page</h2>
+        <UnderConstruction />
+      </div>
+    </Layout>
+  );
+}
