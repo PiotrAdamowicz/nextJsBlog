2cdd540ef4f73d0bb9933b081ef07d22da074a31
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2020-08-03 14:38:02 +0200

    Serverless contact form with Next.js Api Routes and Nodemailer

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,12 @@
 import Layout from "../components/layout";
 import { useForm } from "react-hook-form";
 
+/*
+There will be few branches for contact forms to try out the best.
+From:
+https://stadtteilliebe.de/blog/serverless-contact-form
+*/
+
 // import UnderConstruction from "../components/underConstruction";
 
 const Contact = () => {
