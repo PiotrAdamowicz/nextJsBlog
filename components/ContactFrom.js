0e524be860add11e58a30bd21e4af54e5ffce2c6
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-01-14 19:05:51 +0100

    Form emailer working and ready for validation logic

--- components/ContactFrom.js
+++ components/ContactFrom.js
@@ -1,21 +1,43 @@
+import { useState } from "react";
 import styles from "../styles/contact-form.module.scss";
 import emailjs from "emailjs-com";
-import { useForm, handleSubmit } from "react-hook-form";
 
 function ContactForm() {
-  const { register, errors, handleSubmit } = useForm({ mode: "onChange" });
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
-  };
-  function sendEmail(e) {
-    e.preventDefault();
+  const [name, setName] = useState("your name");
+  const [email, setEmail] = useState("your@email.com");
+  const [message, setMessage] = useState("Type your message...");
 
+  const handleValidation = () => {
+    const state = [name, email, message];
+    console.log(state);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.name);
+    switch (e.target.name) {
+      case "from_name":
+        setName({ name: e.target.value });
+        break;
+      case "user_email":
+        setEmail({ email: e.target.value });
+        break;
+      case "message":
+        setMessage({ message: e.target.value });
+        break;
+    }
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    handleValidation();
+
+    //emailer hook up
     emailjs
       .sendForm(
-        "service_0pah77j",
-        "template_z7s32ym",
-        e.target,
-        "user_YSKQRUHJ4CvGKJhToQaSR"
+        // "service_0pah77j",
+        // "template_z7s32ym",
+        e.target
+        // "user_YSKQRUHJ4CvGKJhToQaSR"
       )
       .then(
         (result) => {
@@ -26,14 +48,14 @@ function ContactForm() {
         }
       );
     //TODO: hide IDs in .env
-  }
+  };
 
   return (
     <form
       className={styles.contact_form}
       acceptCharset="utf-8"
       method="post"
-      // onSubmit={sendEmail}
+      onSubmit={sendEmail}
     >
       <fieldset className={styles.fieldset}>
         <legend className={styles.legend}>
@@ -43,31 +65,31 @@ function ContactForm() {
           Name:
         </label>
         <input
+          onChange={handleChange}
           type="text"
           name="from_name"
           className={styles.input}
-          placeholder="your name"
-          ref={register({ minLenght: 3, maxLenght: 25 })}
+          placeholder={name}
         />
         <label htmlFor="user_email" className={styles.label}>
           Email:
         </label>
         <input
+          onChange={handleChange}
           type="email"
           name="user_email"
           className={styles.input}
-          placeholder="your@email.com"
-          ref={register}
+          placeholder={email}
         />
         <label htmlFor="user_email" className={styles.label}>
           Message:
         </label>
         <textarea
+          onChange={handleChange}
           name="message"
           className={styles.message}
-          placeholder="Type your message..."
+          placeholder={message}
           rows="7"
-          ref={register}
         />
         <input
           type="submit"
