20428a64ce3731a45bccaa6c24c27c81a58d081f
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-02-02 17:23:46 +0100

    Bug fixed

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
 import Layout from "../components/layout";
+import styles from "../styles/about.module.scss";
 import UnderConstruction from "../components/underConstruction";
 
 export default function About() {
   return (
     <Layout>
       <div>
-        <h2>This is the about page</h2>
+        <h2 className={styles.header}>This is the about page</h2>
         <UnderConstruction />
       </div>
     </Layout>
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,18 +1,10 @@
 import Layout from "../components/layout";
+import UnderConstruction from "../components/underConstruction";
 
 const Portfolio = () => {
   return (
     <Layout>
-      <ul>
-        <li>
-          <h2>Tic Tac Toe</h2>
-          <p className="description">
-            Tic tac toe game made on the beginning of my learning journey, using
-            plain JS.
-          </p>
-          <a href="https://piotradamowicz.github.io/TicTacToe/">TicTacToe</a>
-        </li>
-      </ul>
+      <UnderConstruction />
     </Layout>
   );
 };
